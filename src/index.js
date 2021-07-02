f0486abe0dc3ea1114b1cfec21b6476a97486f16
vmik7 <vmik7@yandex.ru>
2021-07-02 23:53:33 +0300

    Realize plugin

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const fsp = require('fs/promises');
+const delay = require('delay');
+
+module.exports = (hermione) => {
+    let selenium;
+
+    hermione.on(hermione.events.RUNNER_START, async () => {
+        const file = await fsp.open('selenium.log', 'w');
+
+        selenium = spawn('selenium-standalone', ['start'], {
+            stdio: ['ignore', file, file],
+        });
+
+        await delay(2000);
+    });
+
+    hermione.on(hermione.events.RUNNER_END, () => {
+        return new Promise((resolve) => {
+            selenium.on('exit', () => resolve());
+
+            selenium.kill();
+        });
+    });
+};
